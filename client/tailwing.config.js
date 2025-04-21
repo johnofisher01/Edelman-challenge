82087df95bd225dc0a55dbf19d89ad7d1ab75c70
Johno <john@mount-media-online.com>
2025-04-21 15:45:49 +0100

    tailwind configure

---
+++ client/tailwing.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#192cef",
+          secondary: "#060a4a",
+          accent: "#3773f4",    
+          background: "#fffeff",
+          textPrimary: "#060a4a", 
+        },
+      },
+    },
+    plugins: [],
+  };
